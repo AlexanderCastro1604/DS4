61f6293178e3bade3cf52f469ea29f55592eff49
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-09-16 19:44:54 -0500

    Subir Laboratorio 2 completo

--- Laboratorio_2/Laboratorio_2/Program.cs
+++ Laboratorio_2/Laboratorio_2/Program.cs
@@ -7,7 +7,7 @@ namespace Laboratorio_2
         static void Main(string[] args)
         {
             Client client = new Client();
-            //Ejemplo utilizando las variables de instancia de Clase.
+            
             client.FirstName = "Su_Nombre";
             client.LastName = "Su_Apellido";
             client.Age = 15;
@@ -19,7 +19,7 @@ namespace Laboratorio_2
 
     public class Client
     {
-        //Declarando variables de instancia en clase.
+       
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -27,7 +27,7 @@ namespace Laboratorio_2
 
         public string GetFullName()
         {
-            //Utilizando variables de instancia dentro de métodos de la clase.
+            
             return FirstName + " " + LastName;
         }
     }
