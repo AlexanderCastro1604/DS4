1557f46c22b84745e054ab51e20c1c3605dc930e
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-09-24 20:13:32 -0500

    Laboratorio 6

---
+++ Laboratorio_6/Laboratorio 6-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado:");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("No se ha introducido un dígito válido.");
+            num = -1;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("El número introducido es muy grande.");
+            num = -1;
+        }
+
+        Console.WriteLine("El número final es: " + num);
+    }
+}
