4832bef86c785970206813f4a3ea931523d1d1d4
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-09-30 19:00:52 -0500

    Laboratorio 7

---
+++ Laboratorio_7/Laboratorio 7-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        JuegoDeDados j = new JuegoDeDados();
+        j.Jugar();
+
+        Console.ReadKey(); 
+    }
+}
+
