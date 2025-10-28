47e926a440c682762aa4cea798006eb365a5b534
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-10-28 04:10:38 -0500

    Lab 9 completo

---
+++ Laboratorio_9/Laboratorio 9-5/Aleatorios.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Aleatorios
+{
+    private Random random;
+
+    public Aleatorios()
+    {
+        random = new Random();
+    }
+
+    
+    public int GenerarEntre2Numeros(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+
+    public int[] GenerarArregloAleatorio(int cantidad, int min, int max)
+    {
+        int[] arreglo = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = random.Next(min, max + 1);
+        }
+
+        return arreglo;
+    }
+
+    public int[] GenerarArregloSinRepetidos(int cantidad, int min, int max)
+    {
+        
+        if (cantidad > (max - min + 1))
+        {
+            throw new ArgumentException("No es posible generar tantos números únicos en el rango dado");
+        }
+
+        HashSet<int> numerosUnicos = new HashSet<int>();
+
+        while (numerosUnicos.Count < cantidad)
+        {
+            int numero = random.Next(min, max + 1);
+            numerosUnicos.Add(numero);
+        }
+
+        return numerosUnicos.ToArray();
+    }
+}
---
+++ Laboratorio_9/Laboratorio 9-5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Aleatorios aleatorios = new Aleatorios();
+        int[] arregloSinRepetidos = aleatorios.GenerarArregloSinRepetidos(10, 1, 50);
+
+        Console.WriteLine($"Arreglo sin repetidos: [{string.Join(", ", arregloSinRepetidos)}]");
+        Console.WriteLine();
+    }
+}
+
