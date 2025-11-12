9e1db477827a08e12a146df3d8b010234d49c46d
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-11-11 20:06:31 -0500

    Lab 12 Completo

---
+++ Laboratorio_12/Laboratorio12_3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AppTriangulo
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSemiperimetro_Click(object sender, EventArgs e)
+        {
+           
+            double ladoA, ladoB, ladoC;
+
+            try
+            {
+                ladoA = Convert.ToDouble(txtA.Text);
+                ladoB = Convert.ToDouble(txtB.Text);
+                ladoC = Convert.ToDouble(txtC.Text);
+
+                double semiP = (ladoA + ladoB + ladoC) / 2;
+                txtResultadoSP.Text = semiP.ToString("F2");
+            }
+            catch
+            {
+                MessageBox.Show("Ingrese valores numéricos correctos en los lados.", "Datos inválidos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            
+            if (double.TryParse(txtA.Text, out double a) &&
+                double.TryParse(txtB.Text, out double b) &&
+                double.TryParse(txtC.Text, out double c))
+            {
+                double sp = (a + b + c) / 2;
+                double resultado = Math.Sqrt(sp * (sp - a) * (sp - b) * (sp - c));
+                txtResultadoArea.Text = $"{resultado:F3}";
+            }
+            else
+            {
+                MessageBox.Show("Los datos ingresados no son válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+        
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                    ctrl.Text = string.Empty;
+            }
+            txtA.Focus();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            
+            this.Dispose();
+        }
+
+       
+        private void txtA_TextChanged(object sender, EventArgs e) { }
+        private void txtB_TextChanged(object sender, EventArgs e) { }
+        private void txtC_TextChanged(object sender, EventArgs e) { }
+        private void txtResultadoSP_TextChanged(object sender, EventArgs e) { }
+        private void txtResultadoArea_TextChanged(object sender, EventArgs e) { }
+        private void lblTitulo_Click(object sender, EventArgs e) { }
+    }
+}
---
+++ Laboratorio_12/Laboratorio_12/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProyectoFisica
+{
+    public partial class FrmCalculoDistancia : Form
+    {
+        public FrmCalculoDistancia()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCalculoDistancia_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = "Cálculo de Distancia (Física Básica)";
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+           
+            if (double.TryParse(txtVelocidad.Text, out double v) &&
+                double.TryParse(txtTiempo.Text, out double t))
+            {
+                double d = v * t;
+                txtResultado.Text = d.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.",
+                                "Error de entrada",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox) ctrl.Text = "";
+            }
+            txtVelocidad.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        private void lblTitulo_Click(object sender, EventArgs e) { }
+        private void txtVelocidad_TextChanged(object sender, EventArgs e) { }
+        private void txtTiempo_TextChanged(object sender, EventArgs e) { }
+        private void txtResultado_TextChanged(object sender, EventArgs e) { }
+    }
+}
