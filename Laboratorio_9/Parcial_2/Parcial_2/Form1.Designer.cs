8098b697b77c7fe419f4112b2f9ac4e153dddc7c
AlexanderCastro1604 <alexandercastroverde6116@gmail.com>
2025-11-11 19:36:59 -0500

    Lab 11 listo

---
+++ Laboratorio_9/Parcial_2/Parcial_2/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Parcial_2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.TextBox txtKelvin;
+        private System.Windows.Forms.Button btnFahrenheit;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.Button btnKelvin;
+        private System.Windows.Forms.ListBox lstHistorial;
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.Label lblKelvin;
+
+        private void InitializeComponent()
+        {
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.txtKelvin = new System.Windows.Forms.TextBox();
+            this.btnFahrenheit = new System.Windows.Forms.Button();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.btnKelvin = new System.Windows.Forms.Button();
+            this.lstHistorial = new System.Windows.Forms.ListBox();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.lblCelsius = new System.Windows.Forms.Label();
+            this.lblKelvin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Location = new System.Drawing.Point(20, 30);
+            this.lblFahrenheit.Text = "Fahrenheit";
+            
+            this.txtFahrenheit.Location = new System.Drawing.Point(100, 25);
+            this.txtFahrenheit.Size = new System.Drawing.Size(100, 27);
+            
+            this.btnFahrenheit.Location = new System.Drawing.Point(220, 25);
+            this.btnFahrenheit.Size = new System.Drawing.Size(40, 27);
+            this.btnFahrenheit.Text = "→";
+            this.btnFahrenheit.Click += new System.EventHandler(this.btnFahrenheit_Click);
+           
+            this.lblCelsius.AutoSize = true;
+            this.lblCelsius.Location = new System.Drawing.Point(20, 80);
+            this.lblCelsius.Text = "Celsius";
+            
+            this.txtCelsius.Location = new System.Drawing.Point(100, 75);
+            this.txtCelsius.Size = new System.Drawing.Size(100, 27);
+            
+            this.btnCelsius.Location = new System.Drawing.Point(220, 75);
+            this.btnCelsius.Size = new System.Drawing.Size(40, 27);
+            this.btnCelsius.Text = "→";
+            this.btnCelsius.Click += new System.EventHandler(this.btnCelsius_Click);
+            
+            this.lblKelvin.AutoSize = true;
+            this.lblKelvin.Location = new System.Drawing.Point(20, 130);
+            this.lblKelvin.Text = "Kelvin";
+            
+            this.txtKelvin.Location = new System.Drawing.Point(100, 125);
+            this.txtKelvin.Size = new System.Drawing.Size(100, 27);
+            
+            
+             
+            this.btnKelvin.Location = new System.Drawing.Point(220, 125);
+            this.btnKelvin.Size = new System.Drawing.Size(40, 27);
+            this.btnKelvin.Text = "→";
+            this.btnKelvin.Click += new System.EventHandler(this.btnKelvin_Click);
+            
+            
+            
+            this.lstHistorial.Location = new System.Drawing.Point(20, 180);
+            this.lstHistorial.Size = new System.Drawing.Size(350, 200);
+            
+             
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.btnFahrenheit);
+            this.Controls.Add(this.lblCelsius);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.lblKelvin);
+            this.Controls.Add(this.txtKelvin);
+            this.Controls.Add(this.btnKelvin);
+            this.Controls.Add(this.lstHistorial);
+            this.Text = "Conversor de Temperaturas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ Laboratorio_9/Parcial_2/Parcial_2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parcial_2
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=ALEXCASTRO1604;Database=TempHist;Trusted_Connection=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFahrenheit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(txtFahrenheit.Text, out double f))
+                {
+                    double c = (f - 32) * 5 / 9;
+                    double k = c + 273.15;
+
+                    txtCelsius.Text = c.ToString("N2");
+                    txtKelvin.Text = k.ToString("N2");
+
+                    string registro = $"{f}°F -> {c}°C, {k}K";
+                    lstHistorial.Items.Add(registro);
+
+                    GuardarConversion(f, c, k, "Fahrenheit");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+        private void btnCelsius_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(txtCelsius.Text, out double c))
+                {
+                    double f = (c * 9 / 5) + 32;
+                    double k = c + 273.15;
+
+                    txtFahrenheit.Text = f.ToString("N2");
+                    txtKelvin.Text = k.ToString("N2");
+
+                    string registro = $"{c}°C -> {f}°F, {k}K";
+                    lstHistorial.Items.Add(registro);
+
+                    GuardarConversion(c, f, k, "Celsius");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+        private void btnKelvin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(txtKelvin.Text, out double k))
+                {
+                    double c = k - 273.15;
+                    double f = (c * 9 / 5) + 32;
+
+                    txtCelsius.Text = c.ToString("N2");
+                    txtFahrenheit.Text = f.ToString("N2");
+
+                    string registro = $"{k}K -> {c}°C, {f}°F";
+                    lstHistorial.Items.Add(registro);
+
+                    GuardarConversion(k, c, f, "Kelvin");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+        
+        private void GuardarConversion(double entrada, double salida1, double salida2, string tipo)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string qry = @"INSERT INTO temp_hist (ValorEntrada, ValorConvertido, TipoConversion, Fecha)
+                               VALUES (@entrada, @convertido, @tipo, GETDATE())";
+
+                SqlCommand cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@entrada", entrada);
+                cmd.Parameters.AddWithValue("@convertido", $"{salida1}, {salida2}"); 
+                cmd.Parameters.AddWithValue("@tipo", tipo);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
