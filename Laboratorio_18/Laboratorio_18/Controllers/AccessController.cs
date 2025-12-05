50a79e777678bc704e1eed733635c3eb628e2acd
Adir <adirjoel30@gmail.com>
2025-12-04 19:44:35 -0500

    Laboratorio 18 Listo

---
+++ Laboratorio_18/Laboratorio_18/Controllers/AccessController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_18.Controllers
+{
+    public class AccessController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+
+    public ActionResult Enter(String User, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error" + ex.Message);
+
+            }
+        }
+    }
+}
---
+++ Laboratorio_18/Laboratorio_18/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Laboratorio_18")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laboratorio_18")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0e981da-a2d8-412b-9565-22da2dd76aeb")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
